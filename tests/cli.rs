fb59b74c264770c05fa3bd3e9c850e5a1bc75dd6
Brendon Bown <bhbochikens@gmail.com>
2020-06-06 17:48:36 -0400

    Added feature tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -4,17 +4,18 @@ use std::path;
 use std::process::Command;
 
 #[test]
-#[ignore]
-fn repl_test() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("tego-lang")?;
-    cmd.arg("repl");
-    unimplemented!()
+fn example_tests() -> Result<(), Box<dyn std::error::Error>> {
+    test_directory("examples")
 }
 
 #[test]
-fn example_tests() -> Result<(), Box<dyn std::error::Error>> {
-    std::env::set_current_dir(path::Path::new(env!("CARGO_MANIFEST_DIR")).join("examples"))?;
-    fs::read_dir(".")?
+fn feature_tests() -> Result<(), Box<dyn std::error::Error>> {
+    test_directory("feature-tests")
+}
+
+fn test_directory<P: AsRef<path::Path>>(path: P) -> Result<(), Box<dyn std::error::Error>> {
+    std::env::set_current_dir(path::Path::new(env!("CARGO_MANIFEST_DIR")))?;
+    fs::read_dir(&path)?
         .filter(|entry| {
             entry
                 .as_ref()
@@ -36,13 +37,20 @@ fn example_tests() -> Result<(), Box<dyn std::error::Error>> {
                         entry.file_name().to_string_lossy().replace(".out", ".tgo");
                     (outfile, codefile)
                 })
+                .map(|(outfile, codefile)| {
+                    let outfile = path.as_ref().join(outfile);
+                    let codefile = path.as_ref().join(codefile);
+                    (outfile, codefile)
+                })
                 .unwrap()
         })
         .for_each(|(outfile, codefile)| {
+            print!("{:?} ... ", codefile);
             let mut cmd = Command::cargo_bin("tego").unwrap();
             let output = cmd.arg("run").arg(codefile).output().unwrap();
             let expected = fs::read_to_string(outfile).unwrap();
             assert_eq!(String::from_utf8(output.stdout).unwrap(), expected);
+            println!("ok");
         });
     Ok(())
 }
