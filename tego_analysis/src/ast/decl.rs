5fa845d3203f073867493113eed8c62cfd85bf20
Brendon Bown <brendon.bown@gmail.com>
2021-11-27 21:31:29 -0700

    Removed the 'tego_analysis' crate

--- tego_analysis/src/ast/decl.rs
+++
@@ -1,6 +0,0 @@
-use crate::ast::{DeclId, ExprId};
-use std::rc::Rc;
-
-pub enum Decl {
-	Expression(String, ExprId)
-}
--- tego_analysis/src/ast/prog.rs
+++
@@ -1,7 +0,0 @@
-use crate::ast::{ProgId, DeclId, ExprId};
-use std::rc::Rc;
-
-pub enum Prog {
-	Library(Vec<DeclId>),
-    Binary(ExprId, Vec<DeclId>),
-}
